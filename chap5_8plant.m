db6ec1c66216ea9f9d519a51540d1c1054709e74
ChopinGuo <guoshaopan@pku.edu.cn>
2018-01-02 16:57:57 +0800

    先进PID控制MATLAB仿真 扩张观测器
    扩张观测器 连续系统仿真

---
+++ chap5_8plant.m
@@ -0,0 +1,52 @@
+function [sys,x0,str,ts] = chap5_8plant(t,x,u,flag)
+
+switch flag
+  case 0
+    [sys,x0,str,ts]=mdlInitializeSizes;
+  case 1
+    sys=mdlDerivatives(t,x,u);
+  case 3
+    sys=mdlOutputs(t,x,u);
+  case {2,4,9}
+    sys=[];
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
+
+function [sys,x0,str,ts]=mdlInitializeSizes
+
+sizes = simsizes;
+
+sizes.NumContStates  = 2;
+sizes.NumDiscStates  = 0;
+sizes.NumOutputs     = 3;
+sizes.NumInputs      = 1;
+sizes.DirFeedthrough = 0;
+sizes.NumSampleTimes = 0;  
+
+sys = simsizes(sizes);
+
+x0  = [0.5;0];
+str = [];
+ts  = [];
+
+function sys=mdlDerivatives(t,x,u)
+
+J = 10;
+ut = u(1);
+
+d = 3.0*sin(t);
+
+sys(1) = x(2);
+sys(2) = 1/J*(ut-d);
+
+function sys=mdlOutputs(t,x,u)
+
+J = 10;
+d = 3.0*sin(t);
+f = -d/J;
+
+sys(1) = x(1);
+sys(2) = x(2);
+sys(3) = f;
